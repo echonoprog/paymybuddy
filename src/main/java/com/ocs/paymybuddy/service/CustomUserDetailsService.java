4b18c717815b84faf212ad02cb07301cc707c83d
echonoprog <a521rr@login.axa>
2023-11-26 10:53:40 +0100

    paymybuddy test ajout postmap : login

--- src/main/java/com/ocs/paymybuddy/service/CustomUserDetailsService.java
+++ src/main/java/com/ocs/paymybuddy/service/CustomUserDetailsService.java
@@ -5,13 +5,12 @@ import com.ocs.paymybuddy.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
+
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
